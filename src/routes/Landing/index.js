8b598aeb30f248fe90ae9415a1ed3587043ab1b6
Adam Wester <awwester@gmail.com>
2020-03-28 16:56:20 -0500

    Add material-ui.

--- src/routes/Landing/index.js
+++ src/routes/Landing/index.js
@@ -1,19 +1,46 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 
-export default function () {
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+export default function ButtonAppBar() {
+  const classes = useStyles();
+  const history = useHistory();
+
+  const handleLoginClick = () => {
+    history.push('/auth');
+  }
+
   return (
-    <div>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/dashboard">Dashboard</Link>
-          </li>
-          <li>
-            <Link to="/auth">Auth</Link>
-          </li>
-        </ul>
-      </nav>
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            Shop Sleuth
+          </Typography>
+          <Button color="inherit" onClick={handleLoginClick}>Login</Button>
+        </Toolbar>
+      </AppBar>
     </div>
-  )
+  );
 }
